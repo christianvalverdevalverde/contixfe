819873dfa20235ea43eb1466641deae0915bd026
christian valverde <christian.valverde@nubesoft.ec>
2025-09-14 01:18:54 -0500

    arranca y tambien managment

--- src/main/scala/cloud/contix/fe/ServicioComprobantesElectronicos.scala
+++ src/main/scala/cloud/contix/fe/ServicioComprobantesElectronicos.scala
@@ -3,12 +3,16 @@ package cloud.contix.fe
 import org.apache.pekko.actor.typed.ActorSystem
 import org.apache.pekko.actor.typed.scaladsl.Behaviors
 import org.apache.pekko.cluster.sharding.typed.scaladsl.ClusterSharding
+import org.apache.pekko.http.scaladsl.model.Uri
+import org.apache.pekko.management.scaladsl.PekkoManagement
+
+import scala.concurrent.Future
 
 object ServicioComprobantesElectronicos {
   
   def main(args: Array[String]): Unit = {
     implicit val actorSystem: ActorSystem[Nothing] =ActorSystem[Nothing](Behaviors.empty,"contixfe")
-   // val pekkoManagment=PekkoManagment(actorSystem).start()
+     PekkoManagement(actorSystem).start()
     val sharding = ClusterSharding(actorSystem)
   }
 }
